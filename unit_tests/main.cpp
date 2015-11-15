84ad94f9fba2425e2e04ef87218c9356a1d46578
Charles Nicholson <charles.nicholson@gmail.com>
2015-11-15 14:14:04 -0800

    add plthook

--- unit_tests/main.cpp
+++ unit_tests/main.cpp
@@ -1,12 +1,20 @@
 #include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTest/TestRegistry.h>
-
 #include <CppUTestExt/MemoryReporterPlugin.h>
+
+extern "C"
+{
+    #include <plthook.h>
+}
 
 int main(int argc, char *argv[])
 {
     MemoryReporterPlugin memoryReporterPlugin;
     TestRegistry::getCurrentRegistry()->installPlugin(&memoryReporterPlugin);
 
-    return CommandLineTestRunner::RunAllTests(argc, argv);
+    plthook_t *plthook = nullptr;
+    auto success = CommandLineTestRunner::RunAllTests(argc, argv);
+
+    plthook_close(plthook);
+    return success;
 }
