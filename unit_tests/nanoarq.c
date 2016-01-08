9a4e627cc878b40b3f65932207bbc76a4d1feef3
Charles Nicholson <charles.nicholson@gmail.com>
2016-01-08 09:11:26 -0800

    update cfg struct, require macros to be defined externally

--- unit_tests/nanoarq.c
+++ unit_tests/nanoarq.c
@@ -1,3 +1,3 @@
 #define NANOARQ_IMPLEMENTATION
-#include "nanoarq.h"
+#include "nanoarq_in_test_project.h"
 
---
+++ unit_tests/nanoarq_in_test_project.h
@@ -0,0 +1,8 @@
+// this file is an example of integrating nanoarq into your project.
+#pragma once
+
+#include <stdint.h>
+#define NANOARQ_UINT32_BASE_TYPE uint32_t
+
+#include "nanoarq.h"
+
