4508438a178d0ee889b5599f906cce1db08cdb9a
Charles Nicholson <charles.nicholson@gmail.com>
2016-02-06 09:19:03 -0800

    separate thunk and target macro expansion out into two separate sections. put all thunks in .text and align to 16 bit. seems to fix mysterious linux-only crash, maybe related to sections / alignment.

--- unit_tests/nanoarq_mocks.cpp
+++ unit_tests/nanoarq_mocks.cpp
@@ -7,14 +7,20 @@
 #endif
 
 #define NANOARQ_MOCK(FUNCTION_NAME) \
-    extern void *FUNCTION_NAME##__; \
-    void *FUNCTION_NAME##_NANOARQ_THUNK_TARGET = &FUNCTION_NAME##__; \
+    extern void *FUNCTION_NAME##_NANOARQ_ORIG; \
+    void *FUNCTION_NAME##_NANOARQ_THUNK_TARGET = &FUNCTION_NAME##_NANOARQ_ORIG;
+
+NANOARQ_MOCK_LIST()
+#undef NANOARQ_MOCK
+
+#define NANOARQ_MOCK(FUNCTION_NAME) \
     __asm( \
-        ".text\n\t"\
-        ".globl " NANOARQ_SYMBOL_PREFIX #FUNCTION_NAME "\n\t"\
-        NANOARQ_SYMBOL_PREFIX #FUNCTION_NAME ":\n\t"\
-        "movq " NANOARQ_SYMBOL_PREFIX #FUNCTION_NAME "_NANOARQ_THUNK_TARGET (%rip), %r11\n\t"\
-        "jmp *%r11\n\t"\
+        ".text\n\t" \
+        ".p2align 4,,15\n\t" \
+        ".globl " NANOARQ_SYMBOL_PREFIX #FUNCTION_NAME "\n\t" \
+        NANOARQ_SYMBOL_PREFIX #FUNCTION_NAME ":\n\t" \
+        "movq " NANOARQ_SYMBOL_PREFIX #FUNCTION_NAME "_NANOARQ_THUNK_TARGET (%rip), %r11\n\t" \
+        "jmp *%r11\n\t" \
     );
 
 NANOARQ_MOCK_LIST()
