2261822407035312604be56c7a25708c296b0e48
Charles Nicholson <charles.nicholson@gmail.com>
2016-03-01 09:04:36 -0800

    include lots of system headers to learn about naming collisions / mistakes

--- compilation_tests/nanoarq_compilation_test.c
+++ compilation_tests/nanoarq_compilation_test.c
@@ -1,11 +1,26 @@
+/* including all of these to ensure no naming collisions in arq.h */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
+#include <assert.h>
+#include <math.h>
+#include <errno.h>
+#include <limits.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <time.h>
+
 #define ARQ_LITTLE_ENDIAN_CPU 1
 #define ARQ_COMPILE_CRC32 1
 
 #if ARQ_USE_C_STDLIB == 0
-    #define ARQ_UINT16_TYPE unsigned short
-    #define ARQ_UINT32_TYPE unsigned int
-    #define ARQ_UINTPTR_TYPE unsigned long
-    #define ARQ_NULL_PTR 0
+    #define ARQ_UINT16_TYPE uint16_t
+    #define ARQ_UINT32_TYPE uint32_t
+    #define ARQ_UINTPTR_TYPE uintptr_t
+    #define ARQ_NULL_PTR NULL
     #define ARQ_MEMCPY(DST, SRC, LEN) __builtin_memcpy((DST), (SRC), (unsigned)(LEN))
 #endif
 
--- compilation_tests/nanoarq_compilation_test.cpp
+++ compilation_tests/nanoarq_compilation_test.cpp
@@ -1,11 +1,34 @@
+// including these to ensure no naming collisions with arq.h
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <map>
+#include <set>
+#include <deque>
+#include <list>
+#include <stack>
+#include <queue>
+#include <new>
+#include <exception>
+#include <stdexcept>
+#include <memory>
+#include <algorithm>
+#include <iterator>
+#include <cstdint>
+#include <cstddef>
+
+using namespace std;
+
 #define ARQ_LITTLE_ENDIAN_CPU 1
 #define ARQ_COMPILE_CRC32 1
 
 #if ARQ_USE_C_STDLIB == 0
-    #define ARQ_UINT16_TYPE unsigned short
-    #define ARQ_UINT32_TYPE unsigned int
-    #define ARQ_UINTPTR_TYPE unsigned long
-    #define ARQ_NULL_PTR 0
+    #define ARQ_UINT16_TYPE uint16_t
+    #define ARQ_UINT32_TYPE uint32_t
+    #define ARQ_UINTPTR_TYPE uintptr_t
+    #define ARQ_NULL_PTR NULL
     #define ARQ_MEMCPY(DST, SRC, LEN) __builtin_memcpy((DST), (SRC), (unsigned)(LEN))
 #endif
 
