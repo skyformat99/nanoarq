9fa5d8a17f166c84575abf163711fc73b38ae8e1
Charles Nicholson <charles.nicholson@gmail.com>
2016-03-07 22:44:34 -0800

    more sketching

--- arq.h
+++ arq.h
@@ -275,7 +275,12 @@ int arq__send_poll(arq__send_wnd_t *sw,
                    arq_time_t rtx,
                    arq_time_t dt);
 
-unsigned arq__recv_wnd_recv(arq__wnd_t *w, unsigned seq, unsigned seg, void const *p, unsigned len);
+unsigned arq__recv_wnd_recv(arq__wnd_t *w,
+                            unsigned seq,
+                            unsigned seg,
+                            unsigned seg_cnt,
+                            void const *p,
+                            unsigned len);
 
 typedef struct arq_t
 {
@@ -887,14 +892,24 @@ int ARQ_MOCKABLE(arq__send_poll)(arq__send_wnd_t *sw,
     return f->len;
 }
 
-unsigned arq__recv_wnd_recv(arq__wnd_t *w, unsigned seq, unsigned seg, void const *p, unsigned len)
+unsigned ARQ_MOCKABLE(arq__recv_wnd_recv)(arq__wnd_t *w,
+                                          unsigned seq,
+                                          unsigned seg,
+                                          unsigned seg_cnt,
+                                          void const *p,
+                                          unsigned len)
 {
-    (void)w;
-    (void)seq;
-    (void)seg;
-    (void)p;
-    (void)len;
-    return 0;
+    void *seg_dst;
+    unsigned seg_dst_len;
+    arq__msg_t *m;
+    ARQ_ASSERT(w && p);
+    arq__wnd_seg(w, seq, seg, &seg_dst, &seg_dst_len);
+    seg_dst_len = arq__min(seg_dst_len, len);
+    ARQ_MEMCPY(seg_dst, p, seg_dst_len);
+    m = &w->msg[seq % w->cap];
+    m->cur_ack_vec |= (1 << seg);
+    m->full_ack_vec = (1 << seg_cnt) - 1;
+    return seg_dst_len;
 }
 
 #if ARQ_COMPILE_CRC32 == 1
