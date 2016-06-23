48d3d05aef9750ea1457edb7f62969b938c07411
Charles Nicholson <charles.nicholson@gmail.com>
2016-06-23 16:56:38 -0700

    cast fp's to void for comparison

--- unit_tests/test_conn_poll_state_cb_get.cpp
+++ unit_tests/test_conn_poll_state_cb_get.cpp
@@ -9,22 +9,26 @@ namespace {
 
 TEST(conn_poll_state_cb_get, closed_returns_closed)
 {
-    CHECK_EQUAL(&arq__conn_poll_state_closed, arq__conn_poll_state_cb_get(ARQ_CONN_STATE_CLOSED));
+    CHECK_EQUAL((void *)&arq__conn_poll_state_closed,
+                (void *)arq__conn_poll_state_cb_get(ARQ_CONN_STATE_CLOSED));
 }
 
 TEST(conn_poll_state_cb_get, rst_sent_returns_rst_sent)
 {
-    CHECK_EQUAL(&arq__conn_poll_state_rst_sent, arq__conn_poll_state_cb_get(ARQ_CONN_STATE_RST_SENT));
+    CHECK_EQUAL((void *)&arq__conn_poll_state_rst_sent,
+                (void *)arq__conn_poll_state_cb_get(ARQ_CONN_STATE_RST_SENT));
 }
 
 TEST(conn_poll_state_cb_get, rst_recvd_returns_rst_recvd)
 {
-    CHECK_EQUAL(&arq__conn_poll_state_rst_recvd, arq__conn_poll_state_cb_get(ARQ_CONN_STATE_RST_RECVD));
+    CHECK_EQUAL((void *)&arq__conn_poll_state_rst_recvd,
+                (void *)arq__conn_poll_state_cb_get(ARQ_CONN_STATE_RST_RECVD));
 }
 
 TEST(conn_poll_state_cb_get, unknown_enums_get_null)
 {
-    CHECK_EQUAL(&arq__conn_poll_state_null, arq__conn_poll_state_cb_get((arq_conn_state_t)9000));
+    CHECK_EQUAL((void *)&arq__conn_poll_state_null,
+                (void *)arq__conn_poll_state_cb_get((arq_conn_state_t)9000));
 }
 
 }
